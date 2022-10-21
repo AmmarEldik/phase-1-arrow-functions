1d4a02c8118e9f9ecf83ca4bc6aa245951fcccb7
AmmarEldik <vektorea60@gmail.com>
2022-10-21 19:54:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let a = 2000; 
+let b = 100; 
+const divide =( a , b)=>{
+    let x = (a / b);
+    return x 
+} 
+ let array = 2;
+const square = (array)=>{
+    let s = (array * array);
+    return s
+}
+let d = 3;
+let f = 4;
+const add = (d,f)=>{
+    let g = (d + f);
+    return g
+}
